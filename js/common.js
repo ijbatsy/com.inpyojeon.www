3bf1205fba8c90116adec8636a2fd759634a26c3
Inpyo Jeon <ijbatsy@gmail.com>
2020-04-09 15:15:20 +0900

    ajax paging

--- js/common.js
+++ js/common.js
@@ -1,10 +1,17 @@
-$(document).on('click', 'a.b', c);
+$(document).on('click', 'a.b', c).on('click', 'nav a', l);
 
 function c(){
 	$('header').toggleClass('on');
-	
 	return false;
 }
 
-$(function(){
-});
+function l(){
+	var d = $(this).data('link') + ".html";
+	$.ajax({ url: d, success: o });
+	return false;
+}
+
+function o(result){
+	$('article').html(result);
+	return false;
+}
